b7903a4f49de04990e24bad202f2f01a6c217ed4
Fran Ruiz <fjavierrule@gmail.com>
2024-02-04 09:31:21 +0100

    intento de herencia

--- Robot8bit/models/items.py
+++ Robot8bit/models/items.py
@@ -4,15 +4,12 @@ import math
 import random
 
 
-class Item():
-    pass
-
-class Diamond (pygame.sprite.Sprite):
+class Item(pygame.sprite.Sprite):
     def __init__(self, game, x, y):
 
         self.game = game
         self._layer = ITEM_LAYER
-        self.groups = self.game.all_sprites, self.game.diamond
+        self.groups = self.game.all_sprites
         pygame.sprite.Sprite.__init__(self, self.groups)
 
         self.x = x * TILESIZE
@@ -20,15 +17,46 @@ class Diamond (pygame.sprite.Sprite):
         self.width = TILESIZE
         self.height = TILESIZE
 
-        image_to_load = pygame.transform.scale(DIAMOND_SKIN, (self.width, self.height))
-
         self.image = pygame.Surface([self.width, self.height])
         self.image.set_colorkey(BLACK)
-        self.image.blit(image_to_load, (0,0))
 
         self.rect = self.image.get_rect()
         self.rect.x = self.x
         self.rect.y = self.y
+
+
+class Diamond (Item):
+    def __init__(self, game, x, y):
+        super().__init__(game, x, y) 
+
+        self.groups.add(self.game.diamond)
+
+        image_to_load = pygame.transform.scale(DIAMOND_SKIN, (self.width, self.height))
+
+        self.image.blit(image_to_load, (0, 0))
+
+# class Diamond (pygame.sprite.Sprite):
+#     def __init__(self, game, x, y):
+#
+#         self.game = game
+#         self._layer = ITEM_LAYER
+#         self.groups = self.game.all_sprites, self.game.diamond
+#         pygame.sprite.Sprite.__init__(self, self.groups)
+#
+#         self.x = x * TILESIZE
+#         self.y = y * TILESIZE
+#         self.width = TILESIZE
+#         self.height = TILESIZE
+#
+#         image_to_load = pygame.transform.scale(DIAMOND_SKIN, (self.width, self.height))
+#
+#         self.image = pygame.Surface([self.width, self.height])
+#         self.image.set_colorkey(BLACK)
+#         self.image.blit(image_to_load, (0,0))
+#
+#         self.rect = self.image.get_rect()
+#         self.rect.x = self.x
+#         self.rect.y = self.y
 
 class Potion (pygame.sprite.Sprite):
     def __init__(self, game, x, y):
