f50f74e0951813f3bc344ed13b4ef9c6d2682a5e
Fran Ruiz <fjavierrule@gmail.com>
2024-02-04 09:49:11 +0100

    herencia hecha

--- Robot8bit/models/items.py
+++ Robot8bit/models/items.py
@@ -27,107 +27,32 @@ class Item(pygame.sprite.Sprite):
 
 class Diamond (Item):
     def __init__(self, game, x, y):
-        super().__init__(game, x, y) 
-
-        self.groups.add(self.game.diamond)
-
+        super().__init__(game, x, y)
+        self.game.diamond.add(self)
         image_to_load = pygame.transform.scale(DIAMOND_SKIN, (self.width, self.height))
-
         self.image.blit(image_to_load, (0, 0))
 
-# class Diamond (pygame.sprite.Sprite):
-#     def __init__(self, game, x, y):
-#
-#         self.game = game
-#         self._layer = ITEM_LAYER
-#         self.groups = self.game.all_sprites, self.game.diamond
-#         pygame.sprite.Sprite.__init__(self, self.groups)
-#
-#         self.x = x * TILESIZE
-#         self.y = y * TILESIZE
-#         self.width = TILESIZE
-#         self.height = TILESIZE
-#
-#         image_to_load = pygame.transform.scale(DIAMOND_SKIN, (self.width, self.height))
-#
-#         self.image = pygame.Surface([self.width, self.height])
-#         self.image.set_colorkey(BLACK)
-#         self.image.blit(image_to_load, (0,0))
-#
-#         self.rect = self.image.get_rect()
-#         self.rect.x = self.x
-#         self.rect.y = self.y
-
-class Potion (pygame.sprite.Sprite):
+class Potion (Item):
     def __init__(self, game, x, y):
-
-        self.game = game
-        self._layer = ITEM_LAYER
-        self.groups = self.game.all_sprites, self.game.potion
-        pygame.sprite.Sprite.__init__(self, self.groups)
-
-        self.x = x * TILESIZE
-        self.y = y * TILESIZE
-        self.width = TILESIZE
-        self.height = TILESIZE
-
+        super().__init__(game, x, y)
+        self.game.potion.add(self)
         image_to_load = pygame.transform.scale(POTION_SKIN, (self.width, self.height))
-
-        self.image = pygame.Surface([self.width, self.height])
-        self.image.set_colorkey(BLACK)
-        self.image.blit(image_to_load, (0,0))
-
-        self.rect = self.image.get_rect()
-        self.rect.x = self.x
-        self.rect.y = self.y
-
-class Wetsuit (pygame.sprite.Sprite):
-    def __init__(self, game, x, y):
-
-        self.game = game
-        self._layer = ITEM_LAYER
-        self.groups = self.game.all_sprites, self.game.wetsuit
-        pygame.sprite.Sprite.__init__(self, self.groups)
-
-        self.x = x * TILESIZE
-        self.y = y * TILESIZE
-        self.width = TILESIZE
-        self.height = TILESIZE
-
-        image_to_load = pygame.transform.scale(WETSUIT_SKIN, (self.width, self.height))
-
-        self.image = pygame.Surface([self.width, self.height])
-        self.image.set_colorkey(BLACK)
         self.image.blit(image_to_load, (0, 0))
 
-        self.rect = self.image.get_rect()
-        self.rect.x = self.x
-        self.rect.y = self.y
-
-
-class Bomb (pygame.sprite.Sprite):
+class Wetsuit (Item):
     def __init__(self, game, x, y):
+        super().__init__(game, x, y)
+        self.game.wetsuit.add(self)
+        image_to_load = pygame.transform.scale(WETSUIT_SKIN, (self.width, self.height))
+        self.image.blit(image_to_load, (0, 0))
 
-        self.game = game
-        self._layer = ITEM_LAYER
-        self.groups = self.game.all_sprites, self.game.bomb
-        pygame.sprite.Sprite.__init__(self, self.groups)
 
-        self.x = x * TILESIZE
-        self.y = y * TILESIZE
-        self.width = TILESIZE
-        self.height = TILESIZE
-
+class Bomb (Item):
+    def __init__(self, game, x, y):
+        super().__init__(game, x, y)
+        self.game.bomb.add(self)
         image_to_load = pygame.transform.scale(BOMB_SKIN, (self.width, self.height))
-
-        self.image = pygame.Surface([self.width, self.height])
-        self.image.set_colorkey(BLACK)
-        self.image.blit(image_to_load, (0,0))
-
-        self.rect = self.image.get_rect()
-        self.rect.x = self.x
-        self.rect.y = self.y
-
+        self.image.blit(image_to_load, (0, 0))
     def collide_with_item(self):
         hits = pygame.sprite.spritecollide(self, self.game.spikes, True)
 
